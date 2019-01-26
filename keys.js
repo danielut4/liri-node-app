b3d37e43a2434431d0c985bc596f295e306a05c4
danielut4 <danielwmorgan@gmail.com>
2019-01-26 16:53:55 -0700

    working

--- keys.js
+++ keys.js
@@ -1,7 +1,6 @@
-js
-console.log('this is loaded');
+console.log("this is loaded");
 
 exports.spotify = {
-  id: process.env.10a7c31b08d74c9dba97f1af69cd952e,
-  secret: process.env.e25953116ef649a9952453c9be4d6aae
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
